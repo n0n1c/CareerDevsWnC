b66887611354aedd12268a3192df3b40c6b8bbb2
Nong Suon <nong.suon@gmail.com>
2017-07-31 17:55:29 +0000

    finished version 7 html and DOM

--- newsAPI/script.js
+++ newsAPI/script.js
@@ -6,10 +6,22 @@ $(document).ready(function() {
         data: data,
         type: "GET",
         success: function(response) {
-            console.log(response);
-            console.log(response.sources[2]);
+            // console.log(response);
+            // console.log(response.sources[2]);
             var sources = response.sources;
-            console.log(sources[2])
+            // console.log(sources[2]);
+            var html = "<select class='form-control' id='source'>";
+            $.each(sources, function(index, source){
+                // console.log(source);
+                html += "<option>" + source.name + "</option>"
+            })
+            html += "</select>";
+            console.log(html)
+            $(".form-group").html(html)
+            
         }
     })
+    function clicked() {
+        
+    }
 })
