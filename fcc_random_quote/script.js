4998143ea3a35acfa2cb5f7f2a0cb3e0055b6a2b
Nong Suon <nong.suon@gmail.com>
2017-08-26 15:28:16 +0000

    Added meanstack folder

---
+++ fcc_random_quote/script.js
@@ -0,0 +1,10 @@
+  var quotes = [
+        "I pretended to be somebody I wanted to be until finally I became that person. Or he became me. -Cary Grant",
+        "Your preparation for the real world is not in the answers you’ve learned, but in the questions you’ve learned how to ask yourself. -Bill Watterson",
+        "Imagination is more important than knowledge. For knowledge is limited to all we now know and understand, while imagination embraces the entire world, and all there ever will be to know and understand. -Albert Einstein",
+        "Once you eliminate the impossible, whatever remains, no matter how improbable, must be the truth. -Sherlock Holmes"
+        ]
+    function newQuote() {
+        var randomNumber = Math.floor * (Math.random() * (quotes.length));
+        document.getElementById('quoteDisplay').innerHTML = quotes[randomNumber];
+    }
